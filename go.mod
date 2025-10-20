8921bddfd8656c8342868e1c9bf47767d1c7caf0
Marian Steinbach <marian@giantswarm.io>
2025-10-20 14:46:21 +0200

    Introduce different dependencies

--- go.mod
+++ go.mod
@@ -1,8 +1,24 @@
 module github.com/giantswarm/renovate-playground
 
-go 1.16
+go 1.21
 
 require (
-	github.com/aws/aws-sdk-go v1.55.0
 	github.com/giantswarm/apiextensions/v3 v3.40.0
+	github.com/santhosh-tekuri/jsonschema/v6 v6.0.0
+)
+
+require (
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+	k8s.io/apimachinery v0.18.19 // indirect
+	k8s.io/klog v1.0.0 // indirect
+	sigs.k8s.io/structured-merge-diff/v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"strings"
+
 	"github.com/giantswarm/apiextensions/v3/pkg/apis/provider/v1alpha1"
+	"github.com/santhosh-tekuri/jsonschema/v6"
 )
 
 func main() {
 	_ = v1alpha1.AzureConfig{}
-	_ = aws.Config{Credentials: nil, Region: nil}
+
+	schema, err := jsonschema.UnmarshalJSON(strings.NewReader(`{"type": "string", "contentEncoding": "hex"}`))
+	if err != nil {
+		log.Fatal(err)
+	}
+	inst := "abcxyz"
+
+	c := jsonschema.NewCompiler()
+	c.RegisterContentEncoding(&jsonschema.Decoder{
+		Name:   "hex",
+		Decode: hex.DecodeString,
+	})
+	c.AssertContent()
+	if err := c.AddResource("schema.json", schema); err != nil {
+		log.Fatal(err)
+	}
+	sch, err := c.Compile("schema.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = sch.Validate(inst)
+	fmt.Println("valid:", err == nil)
 }
